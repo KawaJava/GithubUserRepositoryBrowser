638b402a1973824d692f898747aab58ff06bccdd
kawajava <kawajava0550@gmail.com>
2025-12-23 11:04:30 +0100

    Initial commit

---
+++ src/test/java/io/github/kawajava/GithubUserRepositoryBrowser/GithubUserRepositoryBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kawajava.GithubUserRepositoryBrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserRepositoryBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
